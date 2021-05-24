fe5b8eb3fc2a2a5188113b851103d9c84bea0159
s10th24b <gywn6209@gmail.com>
2021-05-24 19:11:07 +0900

    add foreground service

--- app/src/main/java/com/example/servicetestapplication/MainActivity.kt
+++ app/src/main/java/com/example/servicetestapplication/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.servicetestapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.servicetestapplication.databinding.ActivityMainBinding
+import com.example.servicetestapplication.service.MyService
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.apply {
+            startServiceButton.setOnClickListener {
+                startService()
+            }
+            stopServiceButton.setOnClickListener {
+                stopService()
+            }
+            changeBadgeButton.setOnClickListener {
+                changeBadge()
+            }
+        }
+    }
+
+    fun startService() {
+        val intent = Intent(this, MyService::class.java)
+        intent.action = MyService.ACTION_START
+        Log.d("KHJ", "${startForegroundService(intent)}")
+    }
+
+    fun stopService() {
+        val intent = Intent(this, MyService::class.java)
+        intent.action = MyService.ACTION_STOP
+        Log.d("KHJ", "${stopService(intent)}")
+    }
+    fun changeBadge() {
+        val intent = Intent(this, MyService::class.java)
+        intent.action = MyService.CHANGE_BADGE
+        Log.d("KHJ", "${startForegroundService(intent)}")
     }
 }
--- app/src/main/java/com/example/servicetestapplication/service/MyService.kt
+++ app/src/main/java/com/example/servicetestapplication/service/MyService.kt
@@ -1,24 +1,84 @@
 package com.example.servicetestapplication.service
 
-import android.app.Service
+import android.app.*
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.servicetestapplication.MainActivity
+import com.example.servicetestapplication.R
 
 class MyService : Service() {
+    private lateinit var pendingIntent: PendingIntent
+    private lateinit var notificationChannel: NotificationChannel
+    private lateinit var notification: Notification
+
     companion object {
         const val ACTION_START = "com.example.servicetestapplication.START"
         const val ACTION_RUN = "com.example.servicetestapplication.RUN"
         const val ACTION_STOP = "com.example.servicetestapplication.STOP"
+        const val CHANNEL_ID = "Foreground Channel1"
+        const val CHANGE_BADGE = "CHANGE_BADGE"
+    }
+
+    override fun onCreate() {
+        Log.d("KHJ", "MyService onCreate()")
+        pendingIntent = Intent(this, MainActivity::class.java).let { notificationIntent ->
+            PendingIntent.getActivity(this, 0, notificationIntent, 0)
+        }
+        super.onCreate()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val action = intent?.action
         Log.d("KHJ", "onStartCommand action: $action")
+        when (action) {
+            ACTION_START -> {
+                notificationChannel = createNotificationChannel()
+                notification = createNotification()
+                startForeground(1, notification)
+            }
+        }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun createNotificationChannel(): NotificationChannel {
+        val serviceChannel = NotificationChannel(
+            CHANNEL_ID,
+            "EgoEco App",
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = "연결댓어욤"
+//            setShowBadge(true)
+        }
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(serviceChannel)
+        return serviceChannel
+    }
+
+    private fun createNotification(): Notification {
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("EgoEco Foreground Service")
+            .setContentText("연결댓어요옹")
+            .setContentIntent(pendingIntent)
+            .setTicker("hello")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .build()
+    }
+
+    override fun onDestroy() {
+        Log.d("KHJ", "MyService onDestroy()")
+        super.onDestroy()
     }
 
     override fun onBind(intent: Intent?): IBinder? {
         TODO("Not yet implemented")
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) { // 앱 프로세스 종료시 서비스도 종료.
+        Log.d("KHJ", "onTaskRemoved")
+        stopForeground(true)
+        stopSelf()
+        super.onTaskRemoved(rootIntent)
     }
 }
